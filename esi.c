cc029c49cd8b42082c5f2ed8dad4efbdb42b2727
Frank Jeschke <fjeschke@synapticon.com>
2013-10-04 17:00:21 +0200

    Added new init functions.

--- esi.c
+++ esi.c
@@ -1,4 +1,5 @@
-
+/* ESI - EtherCAT Slave Information
+ */
 #include "esi.h"
 #include "sii.h"
 
@@ -79,17 +80,79 @@ struct _esi_data *esi_init(const char *file)
 		return NULL;
 	}
 
-	enum eFileType type = get_filetype(file);
+	enum eFileType type = efile_type(file);
 	switch (type) {
 	case SIIBIN:
 		/* init with sii bin */
 		esi->sii = sii_init_file(file);
+		if (esi->sii == NULL) {
+			fprintf(stderr, "Failed to read SII bin file '%s'\n", file);
+			free(esi);
+			return NULL;
+		}
 		break;
+
 	case XML:
 		/* init with xml */
+		LIBXML_TEST_VERSION
+
 		esi->sii = sii_init();
 		esi->doc = xmlReadFile(file, NULL, 0);
+		if (esi->doc == NULL) {
+			fprintf(stderr, "Failed to parse XML file '%s'\n", file);
+			free(esi->sii);
+			free(esi);
+			return NULL;
+		}
 		break;
+
+	case UNKNOWN:
+	default:
+		/* init empty ??? */
+		free(esi);
+		esi = NULL;
+		break;
+	}
+
+	return esi;
+}
+
+EsiData *esi_init_string(const unsigned char *buf, size_t size)
+{
+	EsiData *esi = malloc(sizeof(struct _esi_data));
+
+	enum eFileType type = UNKNOWN;
+
+	if (strncmp((const char *)buf, "<?xml", 5) == 0)
+		type = XML;
+	else
+		type = SIIBIN;
+
+	switch (type) {
+	case SIIBIN:
+		/* init with sii bin */
+		esi->sii = sii_init_string(buf, size);
+		if (esi->sii == NULL) {
+			fprintf(stderr, "Failed to read SII bin.\n");
+			free(esi);
+			return NULL;
+		}
+		break;
+
+	case XML:
+		/* init with xml */
+		LIBXML_TEST_VERSION
+
+		esi->sii = sii_init();
+		esi->doc = xmlReadMemory((const char *)buf, size, "newsii.xml", NULL, 0);
+		if (esi->doc == NULL) {
+			fprintf(stderr, "Failed to parse XML.\n");
+			free(esi->sii);
+			free(esi);
+			return NULL;
+		}
+		break;
+
 	case UNKNOWN:
 	default:
 		/* init empty ??? */
@@ -106,4 +169,11 @@ void esi_release(struct _esi_data *esi)
 	sii_release(esi->sii);
 	xmlFreeDoc(esi->doc);
 	free(esi);
+}
+
+void esi_print_xml(EsiData *esi)
+{
+	xmlNode *root = xmlDocGetRootElement(esi->doc);
+	print_all_nodes(root);
+	xmlDocDump(stdout, esi->doc);
 }
--- esi.h
+++ esi.h
@@ -6,9 +6,16 @@
 #ifndef ESI_H
 #define ESI_H
 
+#include <unistd.h>
+
 typedef struct _esi_data EsiData;
 
 EsiData *esi_init(const char *file);
+EsiData *esi_init_file(const char *file);
+EsiData *esi_init_string(const unsigned char *file, size_t size);
+
 void esi_release(EsiData *esi);
+
+void esi_print_xml(EsiData *esi);
 
 #endif /* ESI_H */
