c863c1594436ffd013f129c21dd86bf611ebcdf1
Frank Jeschke <fjeschke@synapticon.com>
2013-10-28 16:25:23 +0100

    Cleaned up.

--- sii.c
+++ sii.c
@@ -13,31 +13,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#if 0
-#define BYTES_TO_WORD(x,y)          ((((int)y<<8)&0xff00) | (x&0xff))
-#define BYTES_TO_DWORD(a,b,c,d)     ((unsigned int)(d&0xff)<<24)  | \
-	                            ((unsigned int)(c&0xff)<<16) | \
-				    ((unsigned int)(b&0xff)<<8)  | \
-				     (unsigned int)(a&0xff)
-#endif
-
-#if 0 /* set in sii.h */
-enum eSection {
-	SII_CAT_NOP
-	,SII_PREAMBLE
-	,SII_STD_CONFIG
-	,SII_CAT_STRINGS = 10
-	,SII_CAT_DATATYPES = 20 /* future use */
-	,SII_CAT_GENERAL = 30
-	,SII_CAT_FMMU = 40
-	,SII_CAT_SYNCM = 41
-	,SII_CAT_TXPDO = 50
-	,SII_CAT_RXPDO = 51
-	,SII_CAT_DCLOCK = 60/* future use */
-	,SII_END = 0xffff
-};
-#endif
-
 static int g_print_offsets = 0;
 
 /* category functions */
@@ -119,29 +94,11 @@ static struct _sii_preamble * parse_preamble(const unsigned char *buffer, size_t
 	preamble->checksum = BYTES_TO_WORD(buffer[count], buffer[count+1]);
 	count += 2;
 
-#if 0
-	/* DEBUG print out */
-	printf("Preamble:\n");
-	printf("PDI Control: %.4x\n", preamble->pdi_ctrl);
-	printf("PDI config: %.4x\n", preamble->pdi_conf);
-	printf("Sync Impulse length = %d ns (raw: %.4x)\n", preamble->sync_impulse*10, preamble->sync_impulse);
-	printf("PDI config 2: %.4x\n", preamble->pdi_conf2);
-	printf("Configured station alias: %.4x\n", preamble->alias);
-	printf("Checksum of preamble: %.4x\n", preamble->checksum);
-#endif
 	if (size != count)
 		printf("%s: Warning counter differs from size\n", __func__);
 
 	return preamble;
 }
-
-#if 0 /* defined in sii.h */
-#define MBOX_EOE    0x0002
-#define MBOX_COE    0x0004
-#define MBOX_FOE    0x0008
-#define MBOX_SOE    0x0010
-#define MBOX_VOE    0x0020
-#endif
 
 static struct _sii_stdconfig *parse_stdconfig(const unsigned char *buffer, size_t size)
 {
@@ -185,43 +142,6 @@ static struct _sii_stdconfig *parse_stdconfig(const unsigned char *buffer, size_
 	b+=2;
 	stdc->version =  BYTES_TO_WORD(*(b+0), *(b+1));
 
-#if 0
-	/* DEBUG print */
-	printf("General Information:\n");
-	printf("Vendor ID: ....... 0x%08x\n", stdc->vendor_id);
-	printf("Product ID: ...... 0x%08x\n", stdc->product_id);
-	printf("Revision ID: ..... 0x%08x\n", stdc->revision_id);
-	printf("Serial Number: ... 0x%08x\n", stdc->serial);
-
-	/* mailbox settings */
-	printf("\nDefault mailbox settings:\n");
-	printf("Bootstrap received mailbox offset: 0x%04x\n", stdc->bs_rec_mbox_offset);
-	printf("Bootstrap received mailbox size:   %d\n", stdc->bs_rec_mbox_size);
-	printf("Bootstrap send mailbox offset:     0x%04x\n", stdc->bs_snd_mbox_offset);
-	printf("Bootstrap send mailbox size:       %d\n", stdc->bs_snd_mbox_size);
-
-	printf("Standard received mailbox offset:  0x%04x\n", stdc->std_rec_mbox_offset);
-	printf("Standard received mailbox size:    %d\n", stdc->std_rec_mbox_size);
-	printf("Standard send mailbox offset:      0x%04x\n", stdc->std_snd_mbox_offset);
-	printf("Standard send mailbox size:        %d\n", stdc->std_snd_mbox_size);
-
-	printf("\nSupported Mailboxes: ");
-	if (stdc->mailbox_protocol.word&MBOX_EOE)
-		printf("EoE, ");
-	if (stdc->mailbox_protocol.word&MBOX_COE)
-		printf("CoE, ");
-	if (stdc->mailbox_protocol.word&MBOX_FOE)
-		printf("FoE, ");
-	if (stdc->mailbox_protocol.word&MBOX_SOE)
-		printf("SoE, ");
-	if (stdc->mailbox_protocol.word&MBOX_VOE)
-		printf("VoE, ");
-	printf("\n");
-
-	printf("EEPROM size: %d kbit\n", stdc->eeprom_size);
-	printf("Version: %d\n", stdc->version);
-	printf("\n");
-#endif
 	count = b-buffer;
 	if (size != count)
 		printf("Warning counter differs from size\n");
@@ -367,38 +287,6 @@ static struct _sii_general *parse_general_section(const unsigned char *buffer, s
 	b++;
 	b+=14;
 
-#if 0
-	/* DEBUG printout */
-	printf("General:\n");
-
-	printf("  Group Index: %d (Vendor Specific, index of Strings): %s\n", siig->groupindex, "tba");
-	printf("  Image Index: %d (Vendor Specific, index to Strings): %s\n", siig->imageindex, "tba");
-	printf("  Order Index: %d (Vendor Specific, index to Strings): %s\n", siig->orderindex, "tba");
-	printf("  Name  Index: %d (Vendor Specific, index to Strings): %s\n", siig->nameindex, "tba");
-
-	printf("  CoE Details:\n");
-	printf("    Enable SDO: .................. %s\n", siig->coe_enable_sdo == 0 ? "no" : "yes");
-	printf("    Enable SDO Info: ............. %s\n", siig->coe_enable_sdo_info == 0 ? "no" : "yes");
-	printf("    Enable PDO Assign: ........... %s\n", siig->coe_enable_pdo_assign == 0 ? "no" : "yes");
-	printf("    Enable PDO Configuration: .... %s\n", siig->coe_enable_pdo_conf == 0 ? "no" : "yes");
-	printf("    Enable Upload at Startup: .... %s\n", siig->coe_enable_upload_start == 0 ? "no" : "yes");
-	printf("    Enable SDO complete access: .. %s\n", siig->coe_enable_sdo_complete == 0 ? "no" : "yes");
-
-	printf("  FoE Details: %s\n", siig->foe_enabled == 0 ? "not enabled" : "enabled");
-	printf("  EoE Details: %s\n", siig->eoe_enabled == 0 ? "not enabled" : "enabled");
-
-
-	printf("  Flag SafeOp: %s\n", siig->flag_safe_op == 0 ? "not enabled" : "enabled");
-	printf("  Flag notLRW: %s\n", siig->flag_notLRW == 0 ? "not enabled" : "enabled");
-
-	printf("  CurrentOnEBus: %d mA\n", siig->current_ebus);
-
-	printf("  Physical Ports:\n");
-	printf("     Port 0: %s\n", physport_type(siig->phys_port_0));
-	printf("     Port 1: %s\n", physport_type(siig->phys_port_1));
-	printf("     Port 2: %s\n", physport_type(siig->phys_port_2));
-	printf("     Port 3: %s\n", physport_type(siig->phys_port_3));
-#endif
 	size_t count = b-buffer;
 	if (size != count)
 		printf("Warning counter differs from size\n");
@@ -446,8 +334,6 @@ void fmmu_add_entry(struct _sii_fmmu *fmmu, int usage)
 
 static struct _sii_fmmu *parse_fmmu_section(const unsigned char *buffer, size_t secsize)
 {
-	//int fmmunbr = 0;
-	//size_t count=0;
 	const unsigned char *b = buffer;
 
 	struct _sii_fmmu *fmmu = malloc(sizeof(struct _sii_fmmu));
@@ -456,28 +342,7 @@ static struct _sii_fmmu *parse_fmmu_section(const unsigned char *buffer, size_t 
 
 	printf("FMMU Settings:\n");
 	while ((unsigned int)(b-buffer)<secsize) {
-		//printf("  FMMU%d: ", fmmunbr++);
 		fmmu_add_entry(fmmu, *b);
-		/*
-		switch (*b) {
-		case 0x00:
-		case 0xff:
-			printf("not used\n");
-			break;
-		case 0x01:
-			printf("used for Outputs\n");
-			break;
-		case 0x02:
-			printf("used for Inputs\n");
-			break;
-		case 0x03:
-			printf("used for SyncM status\n");
-			break;
-		default:
-			printf("WARNING: undefined behavior\n");
-			break;
-		}
-		*/
 		b++;
 	}
 
@@ -579,35 +444,6 @@ static struct _sii_syncm *parse_syncm_section(const unsigned char *buffer, size_
 
 		syncm_add_entry(sm, physadr, length, control, status, enable, type);
 
-#if 0
-		printf("SyncManager SM%d\n", smnbr);
-		printf("  Physical Startaddress: 0x%04x\n", physadr);
-		printf("  Length: %d\n", length);
-		printf("  Control Register: 0x%02x\n", control);
-		printf("  Status Register: 0x%02x\n", status);
-		printf("  Enable byte: 0x%02x\n", enable);
-		printf("  SM Type: ");
-		switch (type) {
-		case 0x00:
-			printf("not used or unknown\n");
-			break;
-		case 0x01:
-			printf("Mailbox Out\n");
-			break;
-		case 0x02:
-			printf("Mailbox In\n");
-			break;
-		case 0x03:
-			printf("Process Data Out\n");
-			break;
-		case 0x04:
-			printf("Process Data In\n");
-			break;
-		default:
-			printf("undefined\n");
-			break;
-		}
-#endif
 		count=(size_t)(b-buffer);
 		smnbr++;
 	}
@@ -689,8 +525,6 @@ static struct _sii_pdo *parse_pdo_section(const unsigned char *buffer, size_t se
 {
 	const unsigned char *b = buffer;
 	char *pdostr;
-	//int pdonbr=0;
-	//int entries = 0;
 	int entry = 0;
 
 	struct _sii_pdo *pdo = malloc(sizeof(struct _sii_pdo));
@@ -724,16 +558,6 @@ static struct _sii_pdo *parse_pdo_section(const unsigned char *buffer, size_t se
 	pdo->flags = BYTES_TO_WORD(*b, *(b+1));
 	b+=2;
 
-#if 0
-	printf("%s%d:\n", pdostr, pdonbr);
-	printf("  PDO Index: 0x%04x\n", pdo->index);
-	printf("  Entries: %d\n", pdo->entries);
-	printf("  SyncM: %d\n", pdo->syncmanager);
-	printf("  Synchronization: 0x%02x\n", pdo->dcsync);
-	printf("  Name Index: %d\n", pdo->name_index);
-	printf("  Flags for future use: 0x%04x\n", pdo->flags);
-#endif
-
 	while ((unsigned int)(b-buffer)<secsize) {
 		int index = BYTES_TO_WORD(*b, *(b+1));
 		b+=2;
@@ -750,15 +574,6 @@ static struct _sii_pdo *parse_pdo_section(const unsigned char *buffer, size_t se
 
 		pdo_add_entry(pdo, index, subindex, string_index, data_type, bit_length, flags);
 
-#if 0
-		printf("\n    Entry %d:\n", entry);
-		printf("    Entry Index: 0x%04x\n", index);
-		printf("    Subindex: 0x%02x\n", subindex);
-		printf("    String Index: %d (%s)\n", string_index, strings[string_index]);
-		printf("    Data Type: 0x%02x (Index in CoE Object Dictionary)\n", data_type);
-		printf("    Bitlength: %d\n", bit_length);
-		printf("     Flags (for future use): 0x%04x\n", flags);
-#endif
 		entry++;
 	}
 
@@ -784,7 +599,6 @@ static struct _sii_dclock *parse_dclock_section(const unsigned char *buffer, siz
 	struct _sii_dclock *dc = malloc(sizeof(struct _sii_dclock));
 	memset(dc, 0, sizeof(struct _sii_dclock));
 
-	//printf("DC Sync Parameter\n");
 	b++; /* first byte is reserved */
 
 	dc->cyclic_op_enabled = (*b & 0x01) == 0 ? 0 : 1;
@@ -837,39 +651,6 @@ static struct _sii_dclock *parse_dclock_section(const unsigned char *buffer, siz
 	dc->latch1_neg_edge_value = BYTES_TO_DWORD(*b, *(b+1), *(b+2), *(b+3));
 	b+=4;
 
-#if 0
-	/* DEBUG printout */
-	printf("  Cyclic Operation Enable: %s\n", dc->cyclic_op_enabled == 0 ? "no" : "yes");
-	printf("  SYNC0 activate: %s\n", dc->sync0_active == 0 ? "no" : "yes");
-	printf("  SYNC1 activate: %s\n", dc->sync1_active == 0 ? "no" : "yes");
-
-	printf("  SYNC Pulse: %d (ns?)\n", dc->sync_pulse);
-
-	printf("  Interrupt 0 Status: %s\n",  dc->int0_status == 0 ? "not active" : "active");
-	printf("  Interrupt 1 Status: %s\n",  dc->int1_status == 0 ? "not active" : "active");
-
-	printf("  Cyclic Operation Startime: %d ns\n", dc->cyclic_op_starttime);
-	printf("  SYNC0 Cycle Time: %d (ns?)\n", dc->sync0_cycle_time);
-	printf("  SYNC0 Cycle Time: %d (ns?)\n", dc->sync1_cycle_time);
-
-	printf("\nLatch Description\n");
-	printf("  Latch 0 PosEdge: %s\n", dc->latch0_pos_edge == 0 ? "continous" : "single");
-	printf("  Latch 0 NegEdge: %s\n", dc->latch0_neg_edge == 0 ? "continous" : "single");
-
-	printf("  Latch 1 PosEdge: %s\n", dc->latch1_pos_edge == 0 ? "continous" : "single");
-	printf("  Latch 1 NegEdge: %s\n", dc->latch1_neg_edge == 0 ? "continous" : "single");
-
-	printf("  Latch 0 PosEvnt: %s\n", dc->latch0_pos_event == 0 ? "no Event" : "Event stored");
-	printf("  Latch 0 NegEvnt: %s\n", dc->latch0_neg_event == 0 ? "no Event" : "Event stored");
-
-	printf("  Latch 1 PosEvnt: %s\n", dc->latch1_pos_event == 0 ? "no Event" : "Event stored");
-	printf("  Latch 1 NegEvnt: %s\n", dc->latch1_neg_event == 0 ? "no Event" : "Event stored");
-
-	printf("  Latch0PosEdgeValue: 0x%08x\n", dc->latch0_pos_edge_value);
-	printf("  Latch0NegEdgeValue: 0x%08x\n", dc->latch0_neg_edge_value);
-	printf("  Latch1PosEdgeValue: 0x%08x\n", dc->latch1_pos_edge_value);
-	printf("  Latch1NegEdgeValue: 0x%08x\n", dc->latch1_neg_edge_value);
-#endif
 	size_t count = b-buffer;
 	if (size != count)
 		printf("Warning counter differs from size\n");
@@ -890,15 +671,12 @@ static void print_offsets(const unsigned char *start, const unsigned char *curre
 static int parse_content(struct _sii *sii, const unsigned char *eeprom, size_t maxsize)
 {
 	enum eSection section = SII_PREAMBLE;
-	//size_t count = 0;
 	size_t secsize = 0;
 	const unsigned char *buffer = eeprom;
 	const unsigned char *secstart = eeprom;
 
 	struct _sii_cat *newcat;
 
-	//struct _sii_preamble *preamble;
-	//struct _sii_stdconfig *stdconfig;
 	struct _sii_strings *strings;
 	struct _sii_general *general;
 	struct _sii_fmmu *fmmu;
