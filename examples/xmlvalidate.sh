e962e327c696b3bc3c1ad4c79b3bbaef332e51bc
Frank Jeschke <fjeschke@synapticon.com>
2013-11-06 11:52:06 +0100

    Changed output

--- examples/xmlvalidate.sh
+++ examples/xmlvalidate.sh
@@ -7,6 +7,6 @@ COMMAND="xmllint --noout --schema ${SCHEME}"
 
 for f in Somanet_CiA402-multi.xml Somanet_CiA402-single.xml Somanet_CtrlProto.xml 
 do
-	echo "Checking $f"
+	#echo "Checking $f"
 	eval ${COMMAND} ${f}
 done
